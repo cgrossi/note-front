5efafdcb6a97c817eaca5e054cbf1ddb6a162e17
cgrossi <cgrossi@gmail.com>
2020-01-08 18:50:07 -0700

    add front end

---
+++ build/precache-manifest.6eb11ff51a58eb288310e20daa6ba5c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31806c01df2978e8ddbf57410a762c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fa9d764467b85bafafc",
+    "url": "/static/css/main.075049df.chunk.css"
+  },
+  {
+    "revision": "f99bb9402a6e27083b8a",
+    "url": "/static/js/2.8bc1dca3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8bc1dca3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fa9d764467b85bafafc",
+    "url": "/static/js/main.273aeb69.chunk.js"
+  },
+  {
+    "revision": "7e4ba3339a49e63b540d",
+    "url": "/static/js/runtime-main.90acc2bd.js"
+  }
+]);
